aa67bab521a292bda4a60b1d1e9c95040addbabd
mikeascalon <mikeascalon@yahoo.com>
2024-01-16 20:50:52 -0800

    pass all but one skip test_three_ones

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -23,6 +23,8 @@ class GameLogic:
         # Add logic to calculate the score based on the provided dice values
         # Replace this with your actual scoring logic
 
+
+
         if len(dice_values) == 1:
             if dice_values[0] == 5:
                 return 50
@@ -31,12 +33,13 @@ class GameLogic:
             elif dice_values[0] == 2:
                 return 0
             elif len(set(dice_values)) == 1 and 1 not in dice_values and 5 not in dice_values:
-                return sum({
-                    (2): 0,
-                    (3): 0,
-                    (4): 0,
-                    (6): 0
-                }.get(tuple(sorted(dice_values)), 0))
+            #     return sum({
+            #         (2): 0,
+            #         (3): 0,
+            #         (4): 0,
+            #         (6): 0
+            #     }.get(tuple(sorted(dice_values)), 0))
+                return 0
             
         elif len(dice_values) == 2:
             if all(value == 5 for value in dice_values):
@@ -46,16 +49,18 @@ class GameLogic:
             elif set(dice_values) == {1, 5}:
                 return 150
             elif any(dice_values.count(value) == 2 for value in set(dice_values)):
-                return sum({
-                    (2, 2): 0,
-                    (3, 3): 0,
-                    (4, 4): 0,
-                    (6, 6): 0
-                }.get(tuple(sorted(dice_values)), 0))
+                # return sum({
+                #     (2, 2): 0,
+                #     (3, 3): 0,
+                #     (4, 4): 0,
+                #     (6, 6): 0
+                # }.get(tuple(sorted(dice_values)), 0))
+                return 0
             
         elif len(dice_values) >= 3:
             count_fives = dice_values.count(5)
             count_ones = dice_values.count(1)
+            counts = {num: dice_values.count(num) for num in set(dice_values)}
             if count_ones == 3 and count_fives == 1:
                 return 1050
             elif count_fives == 3:
@@ -98,4 +103,14 @@ class GameLogic:
                     5: 2000,
                     6: 2400
                 }.get(value, 0) for value in set(dice_values))
+            
+            elif all(count == 2 for count in counts.values()):
+                return 1500
+
+            elif any(dice_values.count(value) < 3 for value in set(dice_values)) and dice_values.count(1) == 0  and dice_values.count(5) == 0:
+                return 0
+            
+
         return sum(dice_values)
+        # return 0
+    
